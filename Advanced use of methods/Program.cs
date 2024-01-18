a20af19ec263de43668efcc03a8e8855cef28809
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-18 12:54:30 -0500

    Delegates

--- Advanced use of methods/Program.cs
+++ Advanced use of methods/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//ProcessString processString1 = TrimTo5Letters;
+//ProcessString processString2 = ToUpper;
+//Console.WriteLine(processString1("Helloooooooooooooooooooo"));
+//Console.WriteLine(processString2("Helloooooooooooooooooooo"));
+//Console.ReadKey();
+
+//string TrimTo5Letters(string input)
+//{
+//	return input.Substring(0, 5);
+//}
+
+//string ToUpper(string input)
+//{
+//	return input.ToUpper();
+//}
+
+//delegate string ProcessString(string input);
+
+Print print1 = text => Console.WriteLine(text.ToUpper());
+Print print2 = text => Console.WriteLine(text.ToLower());
+Print multicast = print1 + print2;
+Print print4 = text => Console.WriteLine(text.Substring(0, 3));
+multicast += print4;
+
+multicast("Crocodile");
+
+Func<string, string, int> sumLengths = SumLengths;
+
+Console.ReadKey();
+
+int SumLengths(string text1, string text2)
+{
+	return text1.Length + text2.Length;
+}
+
+delegate void Print(string input);
