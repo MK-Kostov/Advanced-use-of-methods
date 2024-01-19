8747c6f776a096b2f0eb9fa095688fcb35dbbff2
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-19 08:05:29 -0500

    Dictionary – introduction

--- Advanced use of methods/Program.cs
+++ Advanced use of methods/Program.cs
@@ -16,21 +16,65 @@
 
 //delegate string ProcessString(string input);
 
-Print print1 = text => Console.WriteLine(text.ToUpper());
-Print print2 = text => Console.WriteLine(text.ToLower());
-Print multicast = print1 + print2;
-Print print4 = text => Console.WriteLine(text.Substring(0, 3));
-multicast += print4;
+//Print print1 = text => Console.WriteLine(text.ToUpper());
+//Print print2 = text => Console.WriteLine(text.ToLower());
+//Print multicast = print1 + print2;
+//Print print4 = text => Console.WriteLine(text.Substring(0, 3));
+//multicast += print4;
 
-multicast("Crocodile");
+//multicast("Crocodile");
 
-Func<string, string, int> sumLengths = SumLengths;
+//Func<string, string, int> sumLengths = SumLengths;
 
-Console.ReadKey();
+//Console.ReadKey();
 
-int SumLengths(string text1, string text2)
+//int SumLengths(string text1, string text2)
+//{
+//	return text1.Length + text2.Length;
+//}
+
+//delegate void Print(string input);
+
+var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+var countryToCurrencyMapping = new Dictionary<string, string>
 {
-	return text1.Length + text2.Length;
+	["USA"] = "USD",
+	["India"] = "INR",
+	["Spain"] = "EUR",
+
+};
+
+//var countryToCurrencyMapping = new Dictionary<string, string>
+//{
+//	{ "USA", "USD" },
+//	{ "India", "INR" },
+//	{ "Spain", "EUR" },
+
+//};
+
+//countryToCurrencyMapping.Add("USA", "USD");
+//countryToCurrencyMapping.Add("India", "INR");
+//countryToCurrencyMapping.Add("Spain", "EUR");
+//countryToCurrencyMapping.Add("Italy", "EUR");
+
+//Console.WriteLine("Currency is Spain is " + countryToCurrencyMapping["Spain"]);
+
+countryToCurrencyMapping["Poland"] = "PLN";
+countryToCurrencyMapping["Poland"] = "EUR";
+
+foreach (var countryCurrencyPair in countryToCurrencyMapping)
+{
+	Console.WriteLine(
+		$"Country: {countryCurrencyPair.Key}, " +
+		$"currency: {countryCurrencyPair.Value}");
 }
 
-delegate void Print(string input);
+//Console.WriteLine("Currency is Poland is " + countryToCurrencyMapping["Poland"]);
+
+//if (countryToCurrencyMapping.ContainsKey("Italy"))
+//{
+
+//	Console.WriteLine("Currency is Italy is " + countryToCurrencyMapping["Italy"]);
+//}
+
+Console.ReadKey();
