d53fbad7dc8533a4cf62f4abbbb402c63ad45ea4
Mihail Kostov <mishoka.kostov@gmail.com>
2024-01-22 14:19:49 -0500

    Caching

--- Advanced use of methods/Program.cs
+++ Advanced use of methods/Program.cs
@@ -227,72 +227,88 @@
 //}
 
 
-var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
-var filteringStrategySelector = new FilteringStrategySelector();
+//var numbers = new List<int> { 10, 12, -100, 55, 17, 22 };
+//var filteringStrategySelector = new FilteringStrategySelector();
 
-Console.WriteLine("Select filter:");
-Console.WriteLine(string.Join(Environment.NewLine, filteringStrategySelector.FilteringStrategiesNames));
+//Console.WriteLine("Select filter:");
+//Console.WriteLine(string.Join(Environment.NewLine, filteringStrategySelector.FilteringStrategiesNames));
 
-var userInput = Console.ReadLine();
+//var userInput = Console.ReadLine();
 
-var filteringStrategy = filteringStrategySelector.Select(userInput);
-var result = new Filter().FilterBy(filteringStrategy, numbers);
+//var filteringStrategy = filteringStrategySelector.Select(userInput);
+//var result = new Filter().FilterBy(filteringStrategy, numbers);
 
-Print(result);
+//Print(result);
 
-var words = new[] { "zebra", "ostrich", "otter" };
-var oWords = new Filter().FilterBy(word => word.StartsWith("o"), words);
+//var words = new[] { "zebra", "ostrich", "otter" };
+//var oWords = new Filter().FilterBy(word => word.StartsWith("o"), words);
+
+//Console.ReadKey();
+
+//void Print(IEnumerable<int> numbers)
+//{
+//	Console.WriteLine(string.Join(", ", numbers));
+
+//}
+
+//public class FilteringStrategySelector
+//{
+//	private readonly Dictionary<string, Func<int, bool>> _filteringStrategies = new Dictionary<string, Func<int, bool>>
+//	{
+//		["Even"] = number => number % 2 == 0,
+//		["Odd"] = number => number % 2 == 1,
+//		["Positive"] = number => number > 0,
+//		["Negative"] = number => number < 0,
+//	};
+
+//	public IEnumerable<string> FilteringStrategiesNames => _filteringStrategies.Keys;
+
+//	public Func<int, bool> Select(string filteringType)
+//	{
+//		if (!_filteringStrategies.ContainsKey(filteringType))
+//		{
+//			throw new NotSupportedException($"{filteringType} is not a valid filter");
+//		}
+
+//		return _filteringStrategies[filteringType];
+//	}
+//}
+
+
+//public class Filter
+//{
+//	public IEnumerable<T> FilterBy<T>(Func<T, bool> predicate, IEnumerable<T> numbers)
+//	{
+//		var result = new List<T>();
+
+//		foreach (var number in numbers)
+//		{
+//			if (predicate(number))
+//			{
+//				result.Add(number);
+//			}
+//		}
+
+//		return result;
+//	}
+//}
+
+var dataDownloader = new SlowDataDownloader();
+
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id3"));
+Console.WriteLine(dataDownloader.DownloadData("id1"));
+Console.WriteLine(dataDownloader.DownloadData("id2"));
 
 Console.ReadKey();
 
-void Print(IEnumerable<int> numbers)
+public interface IDataDownloader
 {
-	Console.WriteLine(string.Join(", ", numbers));
-
+	string DownloadData(string resourceId);
 }
-
-public class FilteringStrategySelector
-{
-	private readonly Dictionary<string, Func<int, bool>> _filteringStrategies = new Dictionary<string, Func<int, bool>>
-	{
-		["Even"] = number => number % 2 == 0,
-		["Odd"] = number => number % 2 == 1,
-		["Positive"] = number => number > 0,
-		["Negative"] = number => number < 0,
-	};
-
-	public IEnumerable<string> FilteringStrategiesNames => _filteringStrategies.Keys;
-
-	public Func<int, bool> Select(string filteringType)
-	{
-		if (!_filteringStrategies.ContainsKey(filteringType))
-		{
-			throw new NotSupportedException($"{filteringType} is not a valid filter");
-		}
-
-		return _filteringStrategies[filteringType];
-	}
-}
-
-
-public class Filter
-{
-	public IEnumerable<T> FilterBy<T>(Func<T, bool> predicate, IEnumerable<T> numbers)
-	{
-		var result = new List<T>();
-
-		foreach (var number in numbers)
-		{
-			if (predicate(number))
-			{
-				result.Add(number);
-			}
-		}
-
-		return result;
-	}
-}
-
 
 
 
